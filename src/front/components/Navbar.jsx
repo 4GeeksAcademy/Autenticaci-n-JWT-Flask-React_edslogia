43cc52b35c1f579afe7f369d5acabb33900110f9
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-16 16:15:04 +0000

    creada pagina login, como copia de signup

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -8,9 +8,13 @@ export const Navbar = () => {
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
-				<div className="ml-auto">
+				<div className="ml-auto d-flex justify-content-around">
 					<Link to="/signup">
 						<button className="btn btn-primary">Signup</button>
+					</Link>
+					
+					<Link to="/login">
+						<button className="btn btn-primary mx-2">Login</button>
 					</Link>
 				</div>
 			</div>
